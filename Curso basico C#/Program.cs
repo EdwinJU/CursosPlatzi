864bd42f2eb15cec525df6f8bc7e2b2ce7536943
EdwinJU <ejimenezu@est.utn.ac.cr>
2020-11-06 12:01:05 -0600

    Se agregan archivos del curso practico de html

---
+++ Curso basico C#/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Curso_basico_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+        }
+    }
+}
