740d45d7de1d1ec940009b8b732ecbc827b7c66d
EdwinJU <ejimenezu@est.utn.ac.cr>
2020-11-11 18:54:56 -0600

    Se agregan un ejemplo de MVC con datos

---
+++ ASP-C#/HolaMundo/HolaMundo/Controllers/EscuelaController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using HolaMundo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace HolaMundo.Controllers
+{
+	public class EscuelaController : Controller
+	{
+		public IActionResult Index()
+		{
+			var escuela = new Escuela();
+			escuela.AñoFundacion = 2000;
+			escuela.EscuelaId = Guid.NewGuid().ToString();
+			escuela.Nombre = "Platzi School";
+			return View(escuela);
+		}
+
+	}
+}
---
+++ ASP-C#/HolaMundo/HolaMundo/Models/Escuela.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HolaMundo.Models
+{
+    public class Escuela
+    {
+        public string EscuelaId { get; set; }
+        public string Nombre { get; set; }
+        public int AñoFundacion { get; set; }
+
+    }
+}
